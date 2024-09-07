326e1722285fe1e0790d185f013529543aa61959
Hermi-git <166540756+Hermi-git@users.noreply.github.com>
2024-09-07 21:40:08 +0300

    Time: 87 ms (82.21%) | Memory: 17.1 MB (31.20%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        s_count = Counter(s)
+        for i in range(len(s)):
+            if s_count[s[i]] == 1:
+                return i
+                break
+        return -1
+        
