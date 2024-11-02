b5df7b674be49f67f6cb448ae6d8043a4cf9ea1d
Hermi-git <166540756+Hermi-git@users.noreply.github.com>
2024-11-02 19:39:12 +0300

    Time: 0 ms (100.00%) | Memory: 16.5 MB (67.49%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.py
@@ -0,0 +1,11 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        n1, n2 = 0, 0
+        for num in num1:
+            n1 = n1 * 10 + (ord(num) - ord('0'))
+        for num in num2:
+            n2 = n2 *10 + (ord(num)- ord('0'))
+        
+        ans = n1 * n2
+        return f"{ans}"
+        
