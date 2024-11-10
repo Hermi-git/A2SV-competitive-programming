e80bed6d6f5404dc155cd7cf9aa51e800fdc4ba4
Hermi-git <166540756+Hermi-git@users.noreply.github.com>
2024-11-10 17:26:58 +0300

    Time: 1 ms (77.22%) | Memory: 18.7 MB (87.59%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        single_num = 0
+        for i in nums:
+            single_num ^= i
+        return single_num
+        
