566a8c2799dc80804f47dd3602129ec3a332b294
Hermi-git <166540756+Hermi-git@users.noreply.github.com>
2024-11-19 09:14:26 +0300

    Time: 0 ms (100.00%) | Memory: 16.8 MB (53.34%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,18 +1,61 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         arr2_set = set(arr2)
-        arr1_count = defaultdict(int)
-        last_elt= []
+        arr1_dict = defaultdict(int)
+        end_elt = []
         for n in arr1:
             if n not in arr2_set:
-                last_elt.append(n)
-            arr1_count[n] += 1
-        last_elt.sort()
-
+                end_elt.append(n)
+            arr1_dict[n] += 1
+        end_elt.sort()
         result = []
         for n in arr2:
-            for _ in range(arr1_count[n]):
+            for _ in range(arr1_dict[n]):
                 result.append(n)
+        return result + end_elt
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        # arr1_count = defaultdict(int)
+        # last_elt= []
+        # for n in arr1:
+        #     if n not in arr2_set:
+        #         last_elt.append(n)
+        #     arr1_count[n] += 1
+        # last_elt.sort()
+
+        # result = []
+        # for n in arr2:
+        #     for _ in range(arr1_count[n]):
+        #         result.append(n)
         
-        return result + last_elt
+        # return result + last_elt
 
