3a57d815c30f7df19e8f6d5016d905cb70c3fab1
Hermi-git <166540756+Hermi-git@users.noreply.github.com>
2024-11-19 10:16:46 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (25.39%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,15 +1,9 @@
-from functools import cmp_to_key
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        def compare(a: int, b: int) -> int:
-            if str(a) + str(b) > str(b) + str(a):
-                return -1
-            elif str(a) + str(b) < str(b) + str(a):
-                return 1
-            else:
-                return 0
-        sorted_nums = sorted(nums, key = cmp_to_key(compare))
-        if sorted_nums[0] == 0:
+        str_nums = list(map(str, nums))
+        str_nums.sort(key=lambda x:x*10,reverse = True)
+
+        if str_nums[0] == '0':
             return '0'
-        return ''.join(map(str, sorted_nums))
-            
+        
+        return ''.join(str_nums)
