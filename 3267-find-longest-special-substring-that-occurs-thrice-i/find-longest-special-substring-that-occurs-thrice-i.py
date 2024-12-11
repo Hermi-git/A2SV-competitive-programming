0c9c877f68ec35903d9e3b8fefd3dff03ca6189e
Hermi-git <166540756+Hermi-git@users.noreply.github.com>
2024-12-11 09:34:17 +0300

    Time: 316 ms (5.76%) | Memory: 17.1 MB (19.74%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
@@ -1,20 +1,19 @@
 class Solution:
     def maximumLength(self, s: str) -> int:
-        subarr = []
-        for i in range(len(s)):
-            idx = i
-            while idx < len(s) and s[idx] == s[i]:
-                subarr.append(s[i:idx+1])
-                idx +=1 
-        
-        count = Counter(subarr)
-        longest = 0
-        for sub, n in count.items():
-            if n >= 3:
-                longest = max(len(sub),longest)
-        if longest == 0:
-            return -1
-        return longest
+        result = -1
+        for length in range(1, len(s) + 1):
+            substring_count = defaultdict(int)
+            for i in range(len(s) - length + 1):
+                substring = s[i:i + length]
+                if all(char == substring[0] for char in substring): 
+                    substring_count[substring] += 1
+
+            
+            for substring, count in substring_count.items():
+                if count >= 3:
+                    result = max(result, len(substring))
+
+        return result
 
 
        
