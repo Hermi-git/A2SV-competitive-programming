d66b5fd436e4b793dbd05195e33d634c53fc7581
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2025-10-06 13:06:43 +0300

    Time: 834 ms (20.38%) | Memory: 51.4 MB (38.07%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,18 +1,12 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         memo = [[-1 for _ in range(amount+1)] for _ in range(len(coins))]
-        def dp(index, total):
-            if total > amount:
-                return 0
-            if index == len(coins):
-                if total == amount:
-                    return 1
-                else:
-                    return 0
+        
+        for index in range(len(coins)-1, -1, -1):
+            for total in range(amount, -1, -1):
+                take = memo[index][total + coins[index]] if total + coins[index] <= amount else 0
+                skip = memo[index+1][total] if index +1 != len(coins) else total == amount
+
+                memo[index][total] = take + skip
             
-            if memo[index][total] == -1:
-                memo[index][total] = dp(index, total + coins[index]) + dp(index+1, total)
-            
-            return memo[index][total]
-            
-        return dp(0,0)
+        return memo[0][0]
