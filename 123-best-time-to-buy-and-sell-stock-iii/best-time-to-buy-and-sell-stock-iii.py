c67fa3f9fef472a8b53de00b48243823e7d9fd09
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2025-10-12 22:02:44 +0300

    Time: 2033 ms (7.04%) | Memory: 85.1 MB (14.74%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        memo = [[[-1 for _ in range(3)]for _ in range(2)]for _ in range(len(prices))]
+        def dp(index, can_buy, transaction):
+            if index == len(prices) or transaction == 0:
+                return 0
+        
+            if memo[index][can_buy][transaction] == -1:
+                if can_buy:
+                    buy = -prices[index] + dp(index+1, 0, transaction)
+                    not_buy = dp(index+1, 1, transaction)
+                    profit = max(buy, not_buy)
+                else:
+                    sell = prices[index] +dp(index+1, 1, transaction-1)
+                    not_sell = dp(index+1, 0, transaction)
+                    profit = max(sell, not_sell)
+                memo[index][can_buy][transaction] = profit
+            return memo[index][can_buy][transaction] 
+        return dp(0, 1, 2)
