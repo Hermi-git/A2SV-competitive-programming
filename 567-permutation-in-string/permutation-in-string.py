c15f73c646219cfac5a3c500b5dc8460a9ad4e01
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2025-10-30 21:55:44 +0300

    Time: 44 ms (39.44%) | Memory: 17.8 MB (87.70%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,12 +1,13 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1_count = Counter(s1)
-        s2_count = Counter(s2[:len(s1)])
-        if s1_count == s2_count:
+        s1Count = Counter(s1)
+        s2Count = Counter(s2[:len(s1)])
+        if s1Count == s2Count:
             return True
         for i in range(len(s2) - len(s1)):
-            s2_count[s2[i]] -= 1
-            s2_count[s2[i + len(s1)]] += 1
-            if s1_count == s2_count:
+            s2Count[s2[i]] -= 1
+            s2Count[s2[i+len(s1)]] += 1
+            if s1Count == s2Count:
                 return True
         return False
+        
