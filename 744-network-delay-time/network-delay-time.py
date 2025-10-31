86dc0073a4b344eeb89d8a5e7f93465198004922
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2025-10-31 22:49:46 +0300

    Time: 393 ms (30.76%) | Memory: 19.8 MB (33.16%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -4,18 +4,17 @@ class Solution:
         for u, v , time in times:
             graph[u].append((v, time))
         
-        min_time = [float('inf') for _ in range(n+1)]
-        min_time[k] = 0
+        times = {}
+        heap = [(0, k)]  
 
-        heap = [(0, k)]
         while heap:
-            curr_time, node = heapq.heappop(heap)
-            for v, time in graph[node]:
-                new_time = curr_time + time
-                if new_time < min_time[v]:
-                    min_time[v] = new_time
-                    heapq.heappush(heap, (new_time, v))
-    
-        ans = max(min_time[1:])
-        return ans if ans != float('inf') else -1
-
+            time_k_to_node, node = heapq.heappop(heap)
+            if node in times:
+                continue
+            times[node] = time_k_to_node
+            for neigh, neigh_time in graph[node]:
+                if neigh not in times:
+                    heapq.heappush(heap, (time_k_to_node + neigh_time, neigh))
+        return max(times.values()) if len(times) == n else -1
+        
+       
