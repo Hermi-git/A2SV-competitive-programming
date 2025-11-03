ebd2a67c19f053f57cf5135bae06af414b40c647
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2025-11-03 15:44:55 +0300

    Time: 3 ms (19.56%) | Memory: 17.9 MB (43.65%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        mod  = 10**9 + 7
+        def multiply(a, b, p):
+            return ((a% p) * (b % p)) % p
+
+        def binary_exponent(base, exponent):
+            result = 1
+            while exponent > 0:
+                if exponent & 1:
+                    result = multiply(base, result, mod)
+                base = multiply(base, base, mod)
+                exponent >>= 1
+            return result
+        if n < 0:
+            x = 1 / x       
+            n = -n 
+        
+        even = n - (n//2)
+        odd = n//2
+
+        return (binary_exponent(5, even) * binary_exponent(4, odd)) % mod
