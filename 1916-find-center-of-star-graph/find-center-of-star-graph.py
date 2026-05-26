4b62e8817c18ca39d9f3ad80935160baaa42a46a
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2026-05-26 10:41:54 +0300

    Time: 63 ms (13.65%) | Memory: 56 MB (7.34%) - LeetSync

---
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        for node in graph:
+            if len(graph[node]) == len(edges):
+                return node
