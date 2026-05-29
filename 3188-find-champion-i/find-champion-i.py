52f17a7bb4702c363871addd6a759f08ef579bd8
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2026-05-29 21:50:56 +0300

    Time: 11 ms (19.87%) | Memory: 19.7 MB (97.76%) - LeetSync

---
+++ 3188-find-champion-i/find-champion-i.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findChampion(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        teams = [0 for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] ==1:
+                    teams[i] +=1
+        return teams.index(max(teams))
