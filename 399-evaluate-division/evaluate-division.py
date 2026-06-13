af1050b6b97369f9d4a805457d2ba89edeb114e2
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2026-06-13 17:59:36 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (39.87%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,28 +1,28 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         graph = defaultdict(list)
-        for i, eq in enumerate(equations):
-            u, v = eq
+        for i in range(len(equations)):
+            u = equations[i][0]
+            v = equations[i][1]
             w = values[i]
             graph[u].append((v, w))
             graph[v].append((u, 1/w))
-        print(graph)
-
-        answer = [-1.0] * len(queries)
-        for i, query in enumerate(queries):
-            dividend, divisor = query
-            if dividend not in graph or divisor not in graph:
-                continue
-            q = deque([(dividend, 1)])
-            visited = set()
-            visited.add(dividend)
-            while q:
-                node, w = q.popleft()
-                if node == divisor:
-                    answer[i] = w
-                for neigh, weigh in graph[node]:
-                    if neigh not in visited:
-                        visited.add(neigh)
-                        q.append((neigh, w * weigh))
+        def dfs( src, dst, visited):
+            if src not in graph:
+                return -1
+            if src == dst:
+                return 1
+            visited.add(src)
+            for neigh, weigh in graph[src]:
+                if neigh not in visited:
+                    result = dfs(neigh, dst, visited)
+                    if result != -1:
+                        return weigh * result
+                       
+            return -1
         
-        return answer
+        answer = []
+        for s,d in queries:
+            val = dfs(s, d, set())
+            answer.append(val)
+        return answer 
