becefa6a2084eb7e92c966294c66a3cb5f284935
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2026-07-21 22:03:12 +0300

    Time: 203 ms (28.70%) | Memory: 20.8 MB (13.68%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,25 +1,27 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, -1), (1, 1), (-1, 1), (1, -1)]
-
-        def inbound(row, col):
-            return 0 <= row < n and 0 <= col < n
+        if grid[0][0] == 1 or grid[n-1][n-1] == 1:
+            return -1
+        directions = [(-1, 0), (0, -1), (0, 1), (1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+        def inbound(r, c):
+            return 0 <= r < n and 0 <= c < n
         
-        q = deque([(0, 0, 1)])
-        visited = set((0,0))
-
+        q = deque([(0,0)])
+        visited = set([(0, 0)])
+        nodes = 1
         while q:
-            r, c, length = q.popleft()
-            if grid[r][c]:
-                continue
-            if r == n-1 and c == n-1:
-                return length
-            for dr, dc in directions:
-                new_r = r + dr
-                new_c = c + dc
-                if inbound(new_r, new_c) and (new_r, new_c) not in visited:
-                    q.append((new_r, new_c, length+1))
-                    visited.add((new_r, new_c))
+            for _ in range(len(q)):
+                row, col = q.popleft()
+                if row == n-1 and col == n-1:
+                    return nodes
+                for dr, dc in directions:
+                    nr = row + dr
+                    nc = col + dc 
+                    if inbound(nr, nc) and (nr, nc) not in visited and grid[nr][nc] == 0:
+                        q.append((nr, nc))
+                        visited.add((nr, nc))
+            nodes += 1
         return -1
-                
+    
+
