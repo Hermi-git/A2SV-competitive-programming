0cce2c36790fe5c89d5ee4afe610ef69fba894fd
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2026-07-30 21:09:53 +0300

    Time: 779 ms (30.87%) | Memory: 41.6 MB (89.71%) - LeetSync

---
+++ 833-bus-routes/bus-routes.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        if source == target:
+            return 0
+        stop_to_buses = defaultdict(list)
+        for i in range(len(routes)):
+            for j in range(len(routes[i])):
+                val = routes[i][j]
+                stop_to_buses[val].append(i)
+        starts = stop_to_buses[source]
+        q = deque()
+        visited = set()
+        for start in starts:
+            q.append((start, 1))
+            visited.add(start)
+        while q:
+            bus, bus_count = q.popleft()
+            for stop in routes[bus]:
+                if stop  == target:
+                    return bus_count
+                for bus in stop_to_buses[stop]:
+                    if bus not in visited:
+                        q.append((bus, bus_count+1))
+                        visited.add(bus)
+        
+        return -1
+        
+
