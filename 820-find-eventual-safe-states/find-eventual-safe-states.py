94c5fc6fa7d9c2cf907c4b5daa6010feae5de87e
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2026-08-07 11:56:14 +0300

    Time: 56 ms (39.65%) | Memory: 26 MB (62.71%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,26 +1,27 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        out_graph = [[] for _ in range(len(graph))]
-        out_degree = [0] * len(graph)
-        for node in range(len(graph)):
+        n = len(graph)
+        revese_graph = [[] for _ in range(n)]
+        out_degree = [0] * n
+        for node in range(n):
             for neigh in graph[node]:
-                out_graph[neigh].append(node)
+                revese_graph[neigh].append(node)
                 out_degree[node] += 1
         
         q = deque()
-        for node in range(len(graph)):
+        for node in range(n):
             if out_degree[node] == 0:
                 q.append(node)
-        safe = []
+        safe_nodes = []
         while q:
             node= q.popleft()
-            safe.append(node)
-            for neigh in out_graph[node]:
+            safe_nodes.append(node)
+            for neigh in revese_graph[node]:
                 out_degree[neigh] -= 1
                 if out_degree[neigh] == 0:
                     q.append(neigh)
-        sorted_safe = sorted(safe)
-        return sorted_safe
+        
+        return sorted(safe_nodes)
 
 
 
