354afc81ca353b32d8459cfba7fd025d5af1d2b9
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2026-08-19 17:03:32 +0300

    Time: 58 ms (34.26%) | Memory: 27.9 MB (62.39%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.py
+++ 310-minimum-height-trees/minimum-height-trees.py
@@ -1,6 +1,5 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        
         graph = defaultdict(list)
         degree = [0] * n
         for u, v in edges:
@@ -10,23 +9,18 @@ class Solution:
             degree[v] += 1
         
         q = deque()
-        visited = set()
-        
         for node in range(n):
             if degree[node] == 1:
                 q.append(node)
-                visited.add(node)
         layer = []
         while q:
             order = []
-
             for _ in range(len(q)):
                 node = q.popleft()
                 order.append(node)
                 for neigh in graph[node]:
                     degree[neigh] -= 1
-                    if neigh not in visited and degree[neigh] == 1:
+                    if degree[neigh] == 1:
                         q.append(neigh)
-                        visited.add(neigh)
             layer.append(order)
         return layer[-1] if layer != [] else [0]
